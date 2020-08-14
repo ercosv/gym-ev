d124d0c450c8f5558c41a46e07745b178b33a462
ercosv <36733999+ercosv@users.noreply.github.com>
2020-08-14 23:44:14 +0900

    Update __init__.py

--- gym_ev/__init__.py
+++ gym_ev/__init__.py
@@ -4,7 +4,7 @@ register(
     id='Investment-v1',
     entry_point='gym_ev.env:WalkEnv_ev',
     # left-most and right-most states are terminal
-    kwargs={'n_states': 4500, 'nQ': 500, 'nR': 9},
+    kwargs={'n_states': 500, 'nQ': 100, 'nR': 5},
     max_episode_steps=100,
     reward_threshold=1.0,
     nondeterministic=True,
