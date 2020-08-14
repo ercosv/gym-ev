e1832db1ca2d2e120afb91004d72d47ecd1812ae
ercosv <36733999+ercosv@users.noreply.github.com>
2020-08-14 18:09:35 +0900

    Update __init__.py

--- gym_ev/__init__.py
+++ gym_ev/__init__.py
@@ -1,7 +1,7 @@
 from gym.envs.registration import register
 
 register(
-    id='Investment-v0',
+    id='Investment-v1',
     entry_point='gym_ev.env:WalkEnv_ev',
     # left-most and right-most states are terminal
     kwargs={'n_states': 300, 'nQ': 100, 'nR': 3, 'p11': 0.8., 'p12': 0.15., 'p13': 0.05., 'p21': 0.15., 'p22': 0.7., 'p23': 0.15., 'p31': 0.05., 'p32': 0.15., 'p33': 0.8.},
