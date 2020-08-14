c020a77acc7598ccb4c970d08140c0aa19e03d11
ercosv <36733999+ercosv@users.noreply.github.com>
2020-08-14 13:24:05 +0900

    Update __init__.py

--- gym_ev/__init__.py
+++ gym_ev/__init__.py
@@ -3,7 +3,7 @@ from gym.envs.registration import register
 
 register(
     id='SlipperyWalkSeven_ev-v0',
-    entry_point='gym_walk.envs:WalkEnv',
+    entry_point='gym_ev.env:WalkEnv_ev',
     # left-most and right-most states are terminal
     kwargs={'n_states': 7, 'p_stay': 0.5*2/3., 'p_backward': 0.5*1/3.},
     max_episode_steps=100,
@@ -12,7 +12,7 @@ register(
 )
 register(
     id='Investment-v0',
-    entry_point='gym_walk.envs:WalkEnv',
+    entry_point='gym_ev.env:WalkEnv_ev',
     # left-most and right-most states are terminal
     kwargs={'n_states': 7, 'p_stay': 0.5*2/3., 'p_backward': 0.5*1/3.},
     max_episode_steps=100,
